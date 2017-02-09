bf7ad5568c5133a1cbf7236ccb4eb16f5fc1aed9
HallenM <hellendmitrieva@yandex.ru>
2017-02-10 00:08:51 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <string>
+#include <map>
 #include <vector>
 #include <algorithm>
 #include <cstdio>
@@ -8,13 +9,22 @@ using namespace std;
 
 void main()
 {
-	vector <string> str;
+	vector <string> book;
 	ifstream fin("input.txt");
 	ofstream fout("output.txt");
-	string s;
-	for (int i = 0; fin >> s; i++)
-		str.pushback(s);
-	
+	string str;
+	//запись книги
+	for (int i = 0; fin >> str; i++)
+		book.push_back(str);
+	map <string, int> repeat_words;
+	//проход по всей книге и поиск и подсчёт частоты встречаемости каждого слова
+	for (int i = 0; i < book.size(); i++)
+	{
+		int count = 0;
+		for (int j = 0; j < book.size(); j++)
+			if (book[i] == book[j]) count++;
+		repeat_words.insert(pair<string, int>(book[i], count));
+	}
 
 
 
